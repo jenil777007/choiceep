535b69cbbc76b03b8e908cf66f1be628ed734efe
Jenil Calcuttawala <jenilcalcuttawala@gmail.com>
2018-10-21 20:45:50 +0530

    ADDED | generalised approach to crawl different websites by iterating through URLs set

--- Constants.py
+++ Constants.py
@@ -5,3 +5,18 @@ MONGO_DB_COLLECTION = 'testCollection'
 BASE_URL_FLIPKART = 'https://www.flipkart.com'
 
 NO_OF_ITEMS_TO_CRAWL_THRESHOLD = 10
+
+URLS_TO_CRAWL = {
+    "https://www.flipkart.com/search?q=mobile",
+    "https://www.flipkart.com/search?q=tablets",
+    "https://www.flipkart.com/search?q=cameras",
+    "https://www.flipkart.com/search?q=laptops",
+    "https://www.flipkart.com/search?q=tvs",
+    "https://www.flipkart.com/search?q=headphones",
+    "https://www.flipkart.com/search?q=books",
+    "https://www.flipkart.com/search?q=mens-footwear",
+    "https://www.flipkart.com/search?q=womens-footwear",
+    "https://www.flipkart.com/search?q=mens-clothing",
+    "https://www.flipkart.com/search?q=womens-clothing",
+    "https://www.flipkart.com/search?q=watches"
+}
--- FlipkartCrawler.py
+++ FlipkartCrawler.py
@@ -7,7 +7,8 @@ from MongoClient import *
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 
-url = 'https://www.flipkart.com/search?q=mobile'
+
+# url = 'https://www.flipkart.com/search?q=mobile'
 
 
 # The following function is used to make various requests for different products
@@ -24,58 +25,60 @@ def make_request_and_return_raw_html(request_url):
     return raw_html
 
 
-# The initial point of crawler where different products are listed
-soupContent = soup(make_request_and_return_raw_html(url), "html.parser")
-# Selecting the right part of the html page where outer wrapper of the list is present
-productsWrapper = soupContent.findAll("div", {"class": "_1HmYoV _35HD7C col-10-12"})
-# The exact division where the list of products is located
-products = productsWrapper[0].findAll("div", {"class": "bhgxx2 col-12-12"})
+for url in URLS_TO_CRAWL:
+    # The initial point of crawler where different products are listed
+    soupContent = soup(make_request_and_return_raw_html(url), "html.parser")
+    # Selecting the right part of the html page where outer wrapper of the list is present
+    productsWrapper = soupContent.findAll("div", {"class": "_1HmYoV _35HD7C col-10-12"})
+    # The exact division where the list of products is located
+    products = productsWrapper[0].findAll("div", {"class": "bhgxx2 col-12-12"})
 
-# Extract each product link from the list, fetch raw html of the product page and extract offer details
-for index, product in enumerate(products):
-    if index == 0:
-        continue
-    if index > NO_OF_ITEMS_TO_CRAWL_THRESHOLD:
-        break
+    # Extract each product link from the list, fetch raw html of the product page and extract offer details
+    for index, product in enumerate(products):
+        if index == 0:
+            continue
+        if index > NO_OF_ITEMS_TO_CRAWL_THRESHOLD:
+            break
 
-    productLink = product.findAll("a", {"class": "_31qSD5"})  # type: object
+        productLink = product.findAll("a", {"class": "_31qSD5"})  # type: object
 
-    rawHtmlContentFetchedFromProductLink = soup(
-        make_request_and_return_raw_html(BASE_URL_FLIPKART + productLink[0]["href"]), "html.parser")
-    productDetailsWrapper = rawHtmlContentFetchedFromProductLink.findAll("div", {"class": "_1HmYoV _35HD7C col-8-12"})
-    productCategory = productDetailsWrapper[0].findAll("div", {"class": "_1HEvv0"})[1].a.text
-    offersAndRulesSpans = productDetailsWrapper[0].findAll("span", {"class": "_7g_MLT row"})
+        rawHtmlContentFetchedFromProductLink = soup(
+            make_request_and_return_raw_html(BASE_URL_FLIPKART + productLink[0]["href"]), "html.parser")
+        productDetailsWrapper = rawHtmlContentFetchedFromProductLink.findAll("div",
+                                                                             {"class": "_1HmYoV _35HD7C col-8-12"})
+        productCategory = productDetailsWrapper[0].findAll("div", {"class": "_1HEvv0"})[1].a.text
+        offersAndRulesSpans = productDetailsWrapper[0].findAll("span", {"class": "_7g_MLT row"})
 
-    for idx, span in enumerate(offersAndRulesSpans):
-        if span.li.span.text == "Bank Offer":
-            print("offer")
-            offerDescription = span.li.span.findNext('span').contents[0]
-            print(offerDescription)
+        for idx, span in enumerate(offersAndRulesSpans):
+            if span.li.span.text == "Bank Offer":
+                print("offer")
+                offerDescription = span.li.span.findNext('span').contents[0]
+                print(offerDescription)
 
-            document = dict()
-            # document["siteName"] =
-            document["productCategory"] = productCategory
-            # document["creditCardProvider"] =
-            document["offerDescription"] = offerDescription
+                document = dict()
+                # document["siteName"] =
+                document["productCategory"] = productCategory
+                # document["creditCardProvider"] =
+                document["offerDescription"] = offerDescription
 
-            # if document present in db with above mentioned params
-            #     fetch it from db
-            #     check if the date is same as today's
-            #       if yes then update timestamp to systime and update it to db
-            #       else insert new document
-            # else
-            #     insert document into the db
+                # if document present in db with above mentioned params
+                #     fetch it from db
+                #     check if the date is same as today's
+                #       if yes then update timestamp to systime and update it to db
+                #       else insert new document
+                # else
+                #     insert document into the db
 
-            foundDocuments = find_document_in_db(document)
-            if foundDocuments is not None:
-                for fd in foundDocuments:
-                    document["timeStamp"] = datetime.now()
-                    if fd['timeStamp'].date() == document["timeStamp"].date():
-                        fd['timeStamp'] = datetime.now()
-                        update_timeStamp_of_document_in_db(fd)
-                    else:
-                        insert_document_into_db(document)
-            else:
-                print("Couldn't find the document in the db -> " + str(document))
+                foundDocuments = find_document_in_db(document)
+                if foundDocuments is not None:
+                    for fd in foundDocuments:
+                        document["timeStamp"] = datetime.now()
+                        if fd['timeStamp'].date() == document["timeStamp"].date():
+                            fd['timeStamp'] = datetime.now()
+                            update_timeStamp_of_document_in_db(fd)
+                        else:
+                            insert_document_into_db(document)
+                else:
+                    print("Couldn't find the document in the db -> " + str(document))
 
-                insert_document_into_db(document)
+                    insert_document_into_db(document)
